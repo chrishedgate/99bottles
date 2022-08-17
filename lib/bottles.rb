5000870c16f7753d9c0f3f91da3a7207f52c67c5
Chris Hedgate <chris@hedgate.net>
2022-08-17 15:47:39 +0200

    Replaced if/else with case statement

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -15,26 +15,27 @@ class Bottles
     end
 
     def verse(number)
-        if number > 2
-            "#{number} bottles of beer on the wall, " +
-            "#{number} bottles of beer.\n" +
-            "Take one down and pass it around, " +
-            "#{number-1} bottles of beer on the wall.\n"
-        elsif number == 2
-            "2 bottles of beer on the wall, " +
-            "2 bottles of beer.\n" +
-            "Take one down and pass it around, " +
-            "1 bottle of beer on the wall.\n"
-        elsif number == 1
-            "1 bottle of beer on the wall, " +
-            "1 bottle of beer.\n" +
-            "Take it down and pass it around, " +
-            "no more bottles of beer on the wall.\n"
-        else
+        case number
+        when 0 then
             "No more bottles of beer on the wall, " +
             "no more bottles of beer.\n" +
             "Go to the store and buy some more, " +
             "99 bottles of beer on the wall.\n"
+        when 1 then
+            "1 bottle of beer on the wall, " +
+            "1 bottle of beer.\n" +
+            "Take it down and pass it around, " +
+            "no more bottles of beer on the wall.\n"
+        when 2 then
+            "2 bottles of beer on the wall, " +
+            "2 bottles of beer.\n" +
+            "Take one down and pass it around, " +
+            "1 bottle of beer on the wall.\n"
+        else
+            "#{number} bottles of beer on the wall, " +
+            "#{number} bottles of beer.\n" +
+            "Take one down and pass it around, " +
+            "#{number-1} bottles of beer on the wall.\n"
         end
     end
 end
